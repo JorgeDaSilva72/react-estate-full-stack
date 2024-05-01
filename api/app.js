6a58f31afdc2add0ee75827a21e34d71cbfcbe4f
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-05-01 12:41:50 +0200

    Setting up an express server , the user schema prisma and auth routes and controllers ( register done).

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 
+import authRoute from "./routes/auth.route.js";
+
 const app = express();
 
-app.use("/api/test", (req, res) => {
-  res.send("it works");
-});
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Server is running!");
