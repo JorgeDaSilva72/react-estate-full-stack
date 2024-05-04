19a29bb558338cd989654cf84fa9efae8ab2afe3
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-05-04 10:16:02 +0200

    Adding model Post in schema prisma.

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  // baseURL: "http://localhost:8800/api",
-  baseURL: "https://react-estate-full-stack.onrender.com/api",
+  baseURL: "http://localhost:8800/api",
+  // baseURL: "https://react-estate-full-stack.onrender.com/api",
   withCredentials: true,
 });
 
